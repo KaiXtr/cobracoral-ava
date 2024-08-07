f060b890540d410bfa6008083beab3e90b0ddd7c
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2024-08-06 22:05:59 -0300

    Adicionando páginas novas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "inicio#index"
 
   get "/inicio", to: "inicio#index"
+  get "/instalar", to: "inicio#instalar"
   get "/aprender", to: "inicio#aprender"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
