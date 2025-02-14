a3fe3ec8a4b51cef70698038a23f4e47c59cf639
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2025-02-14 01:38:17 -0300

    Configurando Redis, Turbo e criação de turmas

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,5 @@
 class Usuario < ApplicationRecord
 	validates_uniqueness_of :email
 	scope :all_except, -> (usuario) { where.not(id: usuario) }
+	after_create_commit { broadcast_append_to "usuarios" }
 end
