35dfbcee890f5dbd7334be407e99f77aad53e12b
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2025-02-23 18:20:53 -0300

    Adicionando verificação do Caps Lock

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 
-addEventListener('mouseup', (event) => {
-    let selected_text = window.getSelection();
-    console.log(selected_text);
-})
+function checarCaps(event) {
+    let campoCaps = document.getElementById("campo-caps");
+
+    if (event.getModifierState("CapsLock"))
+        campoCaps.style.visibility = "visible";  
+    else
+        campoCaps.style.visibility = "hidden"; 
+}
+
+addEventListener("keyup", function(event) { 
+    checarCaps(event);
+});
