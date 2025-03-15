7e803b81242cf128f70b34e422e016dd9b7e6767
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2025-03-14 22:54:09 -0300

    Desabilitando acesso a conteúdos por vencimento e liberação

--- app/helpers/conteudos_helper.rb
+++ app/helpers/conteudos_helper.rb
@@ -7,6 +7,35 @@ module ConteudosHelper
         data_hora.strftime("Vence em %d/%m/%Y às %H:%M")
     end
 
+    def is_conteudo_indisponivel(conteudo)
+        dataAgora = DateTime.now
+        dataVencimento = conteudo.data_vencimento
+
+        if dataVencimento then
+            vencimento = (dataVencimento.to_date - dataAgora.to_date).to_i
+
+            if vencimento < 0 then
+                return true
+            else
+                dataLiberacao = conteudo.data_liberacao
+
+                if dataLiberacao then
+                    liberacao = (dataLiberacao.to_date - dataAgora.to_date).to_i
+
+                    if liberacao > 0 then
+                        return true
+                    else
+                        return false
+                    end
+                else
+                    return false
+                end
+            end
+        else
+            return false
+        end
+    end
+
     def conclusao_conteudo(conteudo)
         leitura_conteudo = LeituraConteudo.find_by(
             conteudo_id: conteudo.id,
--- app/helpers/leitura_conteudos_helper.rb
+++ app/helpers/leitura_conteudos_helper.rb
@@ -1,17 +1,39 @@
 module LeituraConteudosHelper
+    def diaOuDias(dias)
+        return (dias == -1) ? ' dia' : ' dias'
+    end
+
     def get_vencimento_restante(conteudo)
         dataAgora = DateTime.now
-        dataConteudo = conteudo.data_vencimento
-        diasRestantes = (dataConteudo.to_date - dataAgora.to_date).to_i
+        dataLiberacao = conteudo.data_liberacao
+        dataVencimento = conteudo.data_vencimento
 
-        if diasRestantes > 0 then
-            return diasRestantes.to_s + ' dias restantes'
+        if !dataLiberacao && !dataVencimento then
+            return ''
         end
-        if diasRestantes == 0 then
-            return 'Vence hoje'
+
+        if dataLiberacao then
+            diasLiberacao = (dataAgora.to_date - dataLiberacao.to_date).to_i
+
+            if diasLiberacao < 0 then
+                return 'Liberado em ' + (-diasLiberacao).to_s + diaOuDias(diasLiberacao)
+            end
         end
-        if diasRestantes < 0 then
-            return 'Atrasado a ' + (diasRestantes * -1).to_s + ' dias'
+
+        if dataVencimento then
+            diasVencimento = (dataVencimento.to_date - dataAgora.to_date).to_i
+
+            if diasVencimento > 0 then
+                return diasVencimento.to_s + diaOuDias(diasVencimento) + ' restantes'
+            end
+            if diasVencimento == 0 then
+                return 'Vence hoje'
+            end
+            if diasVencimento < 0 then
+                return 'Atrasado a ' + (diasVencimento * -1).to_s + diaOuDias(diasVencimento)
+            end
+        else
+            return ''
         end
     end
 end
