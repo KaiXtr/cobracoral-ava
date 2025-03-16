eec14935f65a2e998737f953c3953096e2d9a52d
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2025-03-16 16:52:15 -0300

    Adicionando temas e esquemas de cores

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+//= link themes/theme-default.css
+//= link themes/theme-black.css
