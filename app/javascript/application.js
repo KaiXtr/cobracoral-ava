c2ded00ecd3165469ba636e207c0dd764316ea71
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2025-04-02 03:13:40 -0300

    Adicionando component de contextmenu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,8 +3,50 @@ import "@hotwired/turbo-rails"
 import "trix"
 import "@rails/actiontext"
 
+function showContextMenu() {
+    let cursor = event || window.event;
+    let contextMenuComponent = document.getElementById('context-menu-component');
+
+    if (contextMenuComponent.style.visibility == 'visible')
+        contextMenuComponent.style['transition-duration'] = '.2s';
+
+    contextMenuComponent.style.visibility = 'visible';
+    contextMenuComponent.style.top = cursor.clientY + 'px';
+    contextMenuComponent.style.left = cursor.clientX + 'px';
+
+    let contextMenuOptions = document.getElementById('context-menu-options');
+    contextMenuOptions.style.opacity = '100%';
+}
+
+function hideContextMenu() {
+    let contextMenuComponent = document.getElementById('context-menu-component');
+    contextMenuComponent.style.visibility = 'hidden';
+    contextMenuComponent.style['transition-duration'] = '0s';
+
+    let contextMenuOptions = document.getElementById('context-menu-options');
+    contextMenuOptions.style.opacity = '0%';
+}
+
 const header = document.getElementById('cobracoral-header');
 if (header && document.referrer.includes('entrar')) {
     header.style = "padding-top: 80vw";
     header.classList.add('header-animation');
 }
+
+if (document.addEventListener) {
+    document.addEventListener('contextmenu', function(e) {
+        showContextMenu();
+        e.preventDefault();
+    }, false);
+    document.addEventListener('mousedown', function(e) {
+        hideContextMenu();
+    });
+  } else {
+    document.attachEvent('oncontextmenu', function() {
+        showContextMenu();
+        window.event.returnValue = false;
+    });
+    document.attachEvent('onmousedown', function(e) {
+        hideContextMenu();
+    });
+  }
