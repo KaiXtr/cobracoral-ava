e9a7c131caa453579d4d64a1a5c6d3a5178d52ad
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2025-04-02 15:56:49 -0300

    Implementando ações do context menu

---
+++ app/javascript/context-menu.js
@@ -0,0 +1,97 @@
+let clickedElement;
+
+function loadContextMenuOptions() {
+    let contextMenuOptionSearch = document.getElementById('context-menu-option-search');
+    contextMenuOptionSearch.addEventListener('click', function() {
+        contextMenuSearch();
+    });
+
+    let contextMenuOptionCopy = document.getElementById('context-menu-option-copy');
+    contextMenuOptionCopy.addEventListener('click', function() {
+        contextMenuCopyToClipboard();
+    });
+
+    let contextMenuOptionPaste = document.getElementById('context-menu-option-paste');
+    contextMenuOptionPaste.addEventListener('click', async(evt) => {
+        contextMenuPasteFromClipboard(evt);
+    });
+}
+
+function getSelectedText() {
+    let text;
+
+    if (window.getSelection()) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+
+    return text;
+}
+
+function contextMenuSearch() {
+    if (clickedElement.href) {
+        window.open(clickedElement.href, '_blank').focus();
+    } else if (getSelectedText) {
+        window.open("https://duckduckgo.com?q=" + getSelectedText(), '_blank').focus();
+    }
+}
+
+function contextMenuCopyToClipboard() {
+    navigator.clipboard.writeText(getSelectedText());
+}
+
+async function contextMenuPasteFromClipboard(evt) {
+    let text = await navigator.clipboard.readText();
+    clickedElement.value += text;
+}
+
+function showContextMenu(evt) {
+    clickedElement = evt.originalTarget;
+
+    let cursor = event || window.event;
+    let contextMenuComponent = document.getElementById('context-menu-component');
+
+    if (contextMenuComponent.style.visibility == 'visible')
+        contextMenuComponent.style['transition-duration'] = '.2s';
+
+    contextMenuComponent.style.visibility = 'visible';
+    contextMenuComponent.style.top = cursor.clientY + 'px';
+    contextMenuComponent.style.left = cursor.clientX + 'px';
+
+    let contextMenuOptions = document.getElementById('context-menu-options');
+    contextMenuOptions.style.opacity = '100%';
+}
+
+function hideContextMenu() {
+    let contextMenuComponent = document.getElementById('context-menu-component');
+    contextMenuComponent.style.visibility = 'hidden';
+    contextMenuComponent.style['transition-duration'] = '0s';
+
+    let contextMenuOptions = document.getElementById('context-menu-options');
+    contextMenuOptions.style.opacity = '0%';
+}
+
+if (document.addEventListener) {
+    document.addEventListener('contextmenu', function(e) {
+        showContextMenu(e);
+        e.preventDefault();
+    }, false);
+
+    document.addEventListener('mousedown', function(e) {
+        hideContextMenu();
+    });
+
+    loadContextMenuOptions();
+} else {
+    document.attachEvent('oncontextmenu', function() {
+        showContextMenu(e);
+        window.event.returnValue = false;
+    });
+
+    document.attachEvent('onmousedown', function(e) {
+        hideContextMenu();
+    });
+
+    loadContextMenuOptions();
+}
