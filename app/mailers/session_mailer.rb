420fdb3701df9e0f958033c8f51fd06320e6f622
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2025-04-03 20:52:07 -0300

    Adicionando email de primeiro acesso

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -1,6 +1,26 @@
 class SessionMailer < ApplicationMailer
   default from: "ewertonmatheus2113@gmail.com"
 
+    def primeiro_acesso_email
+        @usuario = params[:usuario]
+
+        if (@usuario.pronomes_usuario == "ela_dela")
+            hashFeminino = Usuario.cargo_usuario_feminino_strings.transform_keys(&:to_s)
+            @cargo_usuario = hashFeminino[@usuario.cargo_usuario + '_feminino_string']
+            @saudacao = "Prezada " + @cargo_usuario
+        else
+            hashMasculino = Usuario.cargo_usuario_masculino_strings.transform_keys(&:to_s)
+            @cargo_usuario = hashMasculino[@usuario.cargo_usuario + '_masculino_string']
+            @saudacao = "Prezado " + @cargo_usuario
+        end
+
+        @senha_provisoria = params[:senha_provisoria]
+        @instituicao_nome = params[:instituicao_nome]
+        @acesso_link = "https://cobracoral-ava.onrender.com/entrar"
+
+        mail(to: @usuario.email, subject: "Primeiro acesso no AVA")
+    end
+
     def acesso_email
         @usuario = params[:usuario]
         @login_device = params[:login_device]
