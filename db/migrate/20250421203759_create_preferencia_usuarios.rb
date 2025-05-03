021273e014b4ee231a64acd1bfc6c056cc901773
Davi Oliveira <davirdoliveira@gmail.com>
2025-05-03 12:48:59 -0300

    Criados valores default para campos da tela de preferência; Adicionado campo quantidade de pomodoro

--- db/migrate/20250421203759_create_preferencia_usuarios.rb
+++ db/migrate/20250421203759_create_preferencia_usuarios.rb
@@ -1,25 +1,26 @@
-  class CreatePreferenciaUsuarios < ActiveRecord::Migration[7.2]
+  class CreatePreferenciaUsuario < ActiveRecord::Migration[7.2]
   def change
-    create_table :preferencia_usuarios do |t|
+    create_table :preferencia_usuario do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :idioma
-      t.string :tema
-      t.boolean :avaliacao_exibir_tempo
-      t.boolean :avaliacao_exibir_progresso
-      t.boolean :pomodoro_ativar
-      t.integer :pomodoro_pomodoris_tempo
-      t.integer :pomodoro_descanso
-      t.boolean :pomodoro_hibernar
-      t.boolean :pomodoro_logoff
-      t.boolean :notificacao_novo_acesso
-      t.boolean :notificacao_comunicados_coordenacao
-      t.boolean :notificacao_comunicados_turma
-      t.boolean :notificacao_agendamentos
-      t.boolean :notificacao_avaliacao_liberada
-      t.boolean :notificacao_conteudo_liberado
-      t.boolean :notificacao_nota_lancada
-      t.boolean :notificacao_nova_mensagem
-      t.boolean :notificacao_situacao_solicitacao
+      t.string :idioma, default: "pt-BR"
+      t.string :tema, default: "default"
+      t.boolean :avaliacao_exibir_tempo, default: true
+      t.boolean :avaliacao_exibir_progresso, default: true
+      t.boolean :pomodoro_ativar, default: true
+      t.integer :pomodoro_pomodoris_tempo, default: 25
+      t.integer :pomodoro_descanso, default: 5
+      t.integer :pomodoro_pomodoris_quant, default: 4
+      t.boolean :pomodoro_hibernar, default: true
+      t.boolean :pomodoro_logoff, default: false
+      t.boolean :notificacao_novo_acesso, default: true
+      t.boolean :notificacao_comunicados_coordenacao, default: true
+      t.boolean :notificacao_comunicados_turma, default: true
+      t.boolean :notificacao_agendamentos, default: true
+      t.boolean :notificacao_avaliacao_liberada, default: true
+      t.boolean :notificacao_conteudo_liberado, default: true
+      t.boolean :notificacao_nota_lancada, default: true
+      t.boolean :notificacao_nova_mensagem, default: true
+      t.boolean :notificacao_situacao_solicitacao, default: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -112,7 +112,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_21_203759) do
     t.index ["usuario_id"], name: "index_matriculas_on_usuario_id"
   end
 
-  create_table "preferencia_usuarios", force: :cascade do |t|
+  create_table "preferencia_usuario", force: :cascade do |t|
     t.integer "usuario_id", null: false
     t.string "idioma"
     t.string "tema"
@@ -121,6 +121,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_21_203759) do
     t.boolean "pomodoro_ativar"
     t.integer "pomodoro_pomodoris_tempo"
     t.integer "pomodoro_descanso"
+    t.integer "pomodoro_pomodoris_quant"
     t.boolean "pomodoro_hibernar"
     t.boolean "pomodoro_logoff"
     t.boolean "notificacao_novo_acesso"
@@ -134,7 +135,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_21_203759) do
     t.boolean "notificacao_situacao_solicitacao"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["usuario_id"], name: "index_preferencia_usuarios_on_user_id"
+    t.index ["usuario_id"], name: "index_preferencia_usuario_on_user_id"
   end
 
   create_table "reacao_comunicados", force: :cascade do |t|
@@ -194,7 +195,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_21_203759) do
   add_foreign_key "leitura_conteudos", "usuarios"
   add_foreign_key "matriculas", "turmas"
   add_foreign_key "matriculas", "usuarios"
-  add_foreign_key "preferencia_usuarios", "usuarios"
+  add_foreign_key "preferencia_usuario", "usuarios"
   add_foreign_key "reacao_comunicados", "comunicados"
   add_foreign_key "reacao_comunicados", "usuarios"
   add_foreign_key "turmas", "cursos"
