a5a9a8c3a28651ce37cc9894a0294eaa5e56a902
Davi Oliveira <davirdoliveira@gmail.com>
2025-05-03 21:02:01 -0300

    Usando usuário autenticado como parâmetro para barra lateral de preferências

--- app/components/barra_lateral_preferencias_component.rb
+++ app/components/barra_lateral_preferencias_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class BarraLateralPreferenciasComponent < ViewComponent::Base
-    def initialize(preferencias_usuario:)
+    def initialize(preferencias_usuario:, usuario_autenticado:)
         @preferencias_usuario = preferencias_usuario
+        @usuario_autenticado = usuario_autenticado
     end
 end
