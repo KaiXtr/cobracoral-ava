387de4724a03658b64db695048db42110ff53e4a
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2025-05-18 16:23:29 -0300

    Alterando design da barra principal e do botão de logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,9 +6,44 @@ import "trix"
 import "controllers"
 import "./context-menu"
 
+function initHeader() {
+    const header = document.getElementById('cobracoral-header');
+
+    if (header) {
+        document.addEventListener('scroll', (e) => {
+            if (window.pageYOffset >= 0) {
+                header.classList.add('header-compacto');
+            } else if (header.classList.contains('header-compacto')) {
+                header.classList.remove('header-compacto');
+            }
+        })
+    
+        let linkSair = document.getElementById('menu-perfil-sair-link');
+        if (linkSair) {
+            linkSair.addEventListener('mouseover', (e) => {
+                let iconOpen = document.getElementById('menu-perfil-sair-open');
+                let iconClose = document.getElementById('menu-perfil-sair-close');
+                
+                iconOpen.style.display = "inline-block";
+                iconClose.style.display = "none";
+                
+            })
+            linkSair.addEventListener('mouseleave', (e) => {
+                let iconOpen = document.getElementById('menu-perfil-sair-open');
+                let iconClose = document.getElementById('menu-perfil-sair-close');
+                
+                iconOpen.style.display = "none";
+                iconClose.style.display = "inline-block";
+            })
+        }
+    }
+}
+
 const header = document.getElementById('cobracoral-header');
 
-if (header && document.referrer.includes('entrar')) {
+if ((header)&&(document.referrer.includes('entrar'))) {
     header.style = "padding-top: 80vw";
     header.classList.add('header-animation');
 }
+
+window.addEventListener('turbo:load', initHeader);
